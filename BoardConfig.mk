6e4de39b2a28ff829d2f94cb58069d0da3c603be
Caleb Gardner <calebq23@gmail.com>
2018-12-26 03:13:57 -0600

    Revert "Some more changes (to try working proper)"
    This reverts commit 6df1b3020a5d7b42831697bea88a36b281878bfe.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,9 +21,13 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno630
 BOARD_BUILD_DISABLED_VBMETAIMAGE := true
 BOARD_USES_VENDORIMAGE := true
 BOARD_USES_QCOM_HARDWARE := true
-#TW_USE_TOOLBOX := true
+TW_USE_TOOLBOX := true
 
-#TARGET_SYSTEM_PROP := $(DEVICE_TREE)/system.prop
+TARGET_SYSTEM_PROP := $(DEVICE_TREE)/system.prop
+
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
 
 USE_RECOVERY_INSTALLER := true
 RECOVERY_INSTALLER_PATH := device/razer/aura/installer
@@ -53,14 +57,14 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.gz-dtb
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Boot image
-BOARD_KERNEL_CMDLINE := earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image skip_override androidboot.fastboot=1
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x0004000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x0004000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE     := 0x0004000000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x0100000000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0BAC1D7000
 BOARD_FLASH_BLOCK_SIZE := 0x40000
@@ -77,11 +81,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 AB_OTA_UPDATER := true
 
 # TWRP specific build flags
-
-BOARD_HAS_NO_REAL_SDCARD := false
-TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
-TW_NO_SCREEN_BLANK := true
-
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
@@ -89,7 +88,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/gadget/lun%d/file"
 #TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
 #TW_BRIGHTNESS_PATH := "/sys/devices/soc/c900000.qcom\x2cmdss_mdp/c900000.qcom\x2cmdss_mdp:qcom\x2cmdss_fb_primary/leds/lcd-backlight/brightness"
-#TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 158
 TW_SCREEN_BLANK_ON_BOOT := true
@@ -97,7 +96,7 @@ TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 BOARD_SUPPRESS_SECURE_ERASE := true
 # Stock kernel sometimes crashes when we toggle MTP
-#TW_EXCLUDE_MTP := true
+TW_EXCLUDE_MTP := true
 
 # We can use the factory reset button combo to enter recovery safely
 TW_IGNORE_MISC_WIPE_DATA := true
@@ -106,17 +105,14 @@ TW_IGNORE_MISC_WIPE_DATA := true
 TW_NO_EXFAT_FUSE := true
 
 # Encryption support
-PLATFORM_SECURITY_PATCH := 2025-12-31
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
-
-#TARGET_HW_DISK_ENCRYPTION := true
-#TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 #TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # Asian region languages
 TW_EXTRA_LANGUAGES := true
 
 # Debug flags
-TWRP_INCLUDE_LOGCAT := true
+#TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
